5ca61607b3104d62d0db0368fe03eb1528d8acf6
Unknown <aportillov@miumg.edu.gt>
2019-02-04 11:25:18 -0600

    Revert "prueba"
    This reverts commit 2b6384af6410690aa774f8647c67668b5b773b03.

--- CapaDatos/Commandos.cs
+++ CapaDatos/Commandos.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +9,6 @@ using System.Windows.Forms;
 
 namespace CapaDatos
 {
-    //esto es una prueba
     public class Commandos
     {
         Conexion nuevo = new Conexion();
